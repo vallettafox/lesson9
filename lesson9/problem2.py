f71f8c67e3307cc5748cfec42f4aafc8368830e0
vallettafox <43784504+vallettafox@users.noreply.github.com>
2018-12-17 23:53:41 -0500

    Add files via upload

---
+++ lesson9/problem2.py
@@ -0,0 +1,12 @@
+print('-'*70)
+print('Locker Bot:')
+print()
+password = input('What is the password? ')
+if password == 'I am Batman':
+	print('Password is correct!')
+	print('Locker opening!')
+else:
+	print('Password invalid...enabling the lasers')
+	print('Pew Pew Pew!')
+	print('Pew Pew Pew!')
+print('-'*70)
---
+++ lesson9/problem3.py
@@ -0,0 +1,9 @@
+print('-'*70)
+print('Alien Greeter Bot:')
+print()
+password = input('What planet are you from? ')
+if password == 'Earth':
+	print('EWWWW I THOUGHT YOU WERE A ALIEN!!!')
+else:
+	print('Welcome to Earth!')
+print('-'*70)
